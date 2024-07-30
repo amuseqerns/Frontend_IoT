8736d6502fe5b04921ce252802dac7e560462fef
amuseqerns <nilobon203@gmail.com>
2024-07-31 00:46:22 +0700

    edit

--- backend-iot/app.py
+++ backend-iot/app.py
@@ -6,8 +6,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 # Import models
-from database import SessionLocal, engine
 import models
+from database import SessionLocal, engine
+
+load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -23,13 +25,11 @@ def get_db():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[""],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=[""],
     allow_headers=["*"],
 )
-
-# https://fastapi.tiangolo.com/tutorial/sql-databases/#crud-utils
 
 @router_v1.get('/books')
 async def get_books(db: Session = Depends(get_db)):
@@ -41,8 +41,18 @@ async def get_book(book_id: int, db: Session = Depends(get_db)):
 
 @router_v1.post('/books')
 async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
-    # TODO: Add validation
-    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'], description=book['description'], synopsis=book['synopsis'], category=book['category'])
+    newbook = models.Book(
+        title=book['title'], 
+        author=book['author'], 
+        year=book['year'], 
+        is_published=book['is_published'],
+        description=book['description'], 
+        prologue=book['prologue'], 
+        type1=book['type1'],
+        type2=book['type2'], 
+        type3=book['type3'], 
+        type4=book['type4']
+    )
     db.add(newbook)
     db.commit()
     db.refresh(newbook)
@@ -50,27 +60,34 @@ async def create_book(book: dict, response: Response, db: Session = Depends(get_
     return newbook
 
 @router_v1.patch('/books/{book_id}')
-async def update_book(book_id: int, response: Response, book: dict, db: Session = Depends(get_db)):
-    update_book = db.query(models.Book).filter(models.Book.id == book_id).first()
-    update_book.title = book['title']
-    update_book.author = book['author']
-    update_book.year = book['year']
-    update_book.is_published = book['is_published']
-    update_book.description = book['description']
-    update_book.synopsis = book['synopsis']
-    update_book.category = book['category']
-    db.commit()
-    db.refresh(update_book)
-    response.status_code = 200
-    return update_book
+async def update_book(book_id: int, book: dict, response: Response, db: Session = Depends(get_db)):
+    currentbook = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if currentbook:
+        currentbook.title = book['title']
+        currentbook.author = book['author']
+        currentbook.year = book['year']
+        currentbook.description = book['description']
+        currentbook.prologue = book['prologue']
+        currentbook.type1 = book['type1']
+        currentbook.type2 = book['type2']
+        currentbook.type3 = book['type3']
+        currentbook.type4 = book['type4']
+        currentbook.is_published = book['is_published']
+
+        db.commit()
+        db.refresh(currentbook)
+        response.status_code = 202
+        return currentbook
+    else:
+        response.status_code = 404
+        return {'message': 'book not found'}
 
 @router_v1.delete('/books/{book_id}')
-async def delete_book(book_id: int, response: Response, db: Session = Depends(get_db)):
+async def delete_book(book_id: int, db: Session = Depends(get_db)):
     book = db.query(models.Book).filter(models.Book.id == book_id).first()
     db.delete(book)
     db.commit()
-    response.status_code = 204
-    return
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String,Float
 
 app.include_router(router_v1)
 
--- backend-iot/models.py
+++ backend-iot/models.py
@@ -4,14 +4,16 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base
 
 class Book(Base):
-    __tablename__ = 'books'
+    tablename = 'books'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author = Column(String, index=True)
     year = Column(Integer, index=True)
-    is_published = Column(Boolean, index=True)
     description = Column(String, index=True)
-    synopsis = Column(String, index=True)
-    category = Column(String, index=True)
-
+    prologue = Column(String, index=True)
+    type1 = Column(String, index=True)
+    type2 = Column(String, index=True)
+    type3 = Column(String, index=True)
+    type4 = Column(String, index=True)
+    is_published = Column(Boolean, index=True)
